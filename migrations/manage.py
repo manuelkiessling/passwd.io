8f32719e54db0df6a0080cdac50f1805b98e58b4
Manuel Kiessling <manuel@kiessling.net>
2012-07-26 11:12:32 +0200

    switched to pg in config files

--- migrations/manage.py
+++ migrations/manage.py
@@ -2,4 +2,4 @@
 from migrate.versioning.shell import main
 
 if __name__ == '__main__':
-    main(url='sqlite:///OnlinePasswordSafe.db', debug='False', repository='./migrations')
+    main(url='postgresql://passwdio:Jnx725Frs09Hmc628Tfa42jM@127.0.0.1/passwdio', debug='False', repository='./migrations')
