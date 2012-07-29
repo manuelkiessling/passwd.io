eb422213cfebdeef1ef384ec59e32c0fe77f9c27
Manuel Kiessling <manuel@kiessling.net>
2012-07-29 15:08:44 +0200

    first version homepage

---
+++ onlinepasswordsafe/static/homepage/make.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+cat vendor/bootstrap*.css app.css > temp.css
+yui-compressor temp.css > index.css
+rm temp.css
+cat vendor/jquery-1.7.1.min.js vendor/bootstrap.js app.js > temp.js
+yui-compressor temp.js > index.js
+rm temp.js
+
