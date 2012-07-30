c06f1ea68d60415ea99f92c48d7b02cff7b70d31
Manuel Kiessling <manuel@kiessling.net>
2012-07-30 08:28:46 +0200

    made assets versionable

--- onlinepasswordsafe/static/webapp/make.sh
+++ onlinepasswordsafe/static/webapp/make.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 cat vendor/jquery.mobile-1.1.1.min.css app.css > temp.css
-yui-compressor temp.css > index.css
+yui-compressor temp.css > index.1.css
 rm temp.css
 cat vendor/jquery-1.7.1.min.js vendor/jquery.mobile-1.1.1.min.js vendor/sjcl.js app.js > temp.js
-yui-compressor temp.js > index.js
+yui-compressor temp.js > index.1.js
 rm temp.js
 
