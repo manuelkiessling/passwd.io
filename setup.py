b61cfc00dfb042d1d0530ccfc96117da140aed1c
Manuel Kiessling <manuel@kiessling.net>
2013-04-28 03:26:34 +0300

    removed falsely fixed version

--- setup.py
+++ setup.py
@@ -7,7 +7,7 @@ README = open(os.path.join(here, 'README.txt')).read()
 CHANGES = open(os.path.join(here, 'CHANGES.txt')).read()
 
 requires = [
-    'pyramid==1.4.1',
+    'pyramid',
     'SQLAlchemy',
     'sqlalchemy-migrate',
     'psycopg2',
